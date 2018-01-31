dab63a4d48dd5d6107f6267c9ce56b0c1027e5bf
pasenau <miguel@cimne.upc.edu>
2018-01-31 18:23:40 +0100

    removing unused packages adding root route

--- jedi001/src/app/routes.ts
+++ jedi001/src/app/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router'
 
 export const routes: Routes = [
      // lazy loading, nomes baixem el modul que es fa servir
+    { path: '', loadChildren: './about-project/about-project.module#AboutProjectModule'},
     { path: 'login', loadChildren: './login/login.module#LoginModule'},
     { path: 'sign-in', loadChildren: './sign-in/sign-in.module#SignInModule'},
     { path: '404', loadChildren: './not-found/not-found.module#NotFoundModule'}, // si fos comoponents, es baixarien totes les components
