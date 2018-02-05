49b23324c0dc04e6731d4b4a892857092d297e2c
pasenau <miguel@cimne.upc.edu>
2018-02-05 19:28:35 +0100

    about (default) page with login button if not already logged in

--- jedi001/src/app/about-project/about-project.component.ts
+++ jedi001/src/app/about-project/about-project.component.ts
@@ -3,6 +3,8 @@ import AboutProject from '../_models/about-project.model'
 /* without {} because we've done an 'export default class ...' */
 import AuthorInfo from '../_models/author-info.model'
 import SocialLink from '../_models/social-link.model'
+import { AuthService } from '../_shared/_services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-about-project',
@@ -10,19 +12,25 @@ import SocialLink from '../_models/social-link.model'
   styleUrls: ['./about-project.component.less']
 })
 export class AboutProjectComponent implements OnInit {
+  loginUrl = '/login'
 
   title = 'my app'
 
-  aboutProject: AboutProject = new AboutProject( 'Jedi card API', 'initial Angular 5 demo app', 'with several tests and dirty code')
+  aboutProject: AboutProject = new AboutProject( 'Jedi card API',
+      'initial Angular 5 demo app',
+      'by Miguel Pasenau http://github.com/pasenau')
   aP: any = {
     title: 'Jedi car API',
-    subtitle: '...'
+    subtitle: 'by Miguel Pasenau'
   } /* con esto no nos autocompleta al hacer this.aP.(...) */
   authorId = 0
 
   lstAuthors: AuthorInfo[] = []
 
-  constructor() { }
+  constructor(
+    private _auth: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -37,4 +45,13 @@ export class AboutProjectComponent implements OnInit {
     'born in ' + ( 1900 + id * id),
   [ new SocialLink( 'facebook', 'fb://author_' + id), new SocialLink( 'twiter', 'tw://author_' + id)]))
   }
+
+  isLogged() {
+    return this._auth.isLogged()
+  }
+
+  onRedirectToLogin() {
+    this._router.navigateByUrl( this.loginUrl)
+  }
+
 }
